9c3caf44d741da70fb306dd3e5fb5ee160cd7953
John Long <jlcomp45@gmail.com>
2020-02-07 10:55:23 -0600

    chapter 5 created add book form and successfully added books

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('libraries/', list_libraries, name='libraries'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
-    # path('book/form', book_form, name='book_form'),
+    path('book/form', book_form, name='book_form'),
 ]
--- libraryapp/views/books/list.py
+++ libraryapp/views/books/list.py
@@ -1,5 +1,7 @@
 import sqlite3
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import reverse
 from libraryapp.models import Book
 from libraryapp.models import model_factory
 from ..connection import Connection
@@ -46,3 +48,20 @@ def book_list(request):
         }
 
         return render(request, template, context)
+
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
+
+            db_cursor.execute(""" 
+            INSERT INTO libraryapp_book
+            (
+                title, author, ISBN_number, year_published, location_id, librarian_id
+            )
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+            (form_data['title'], form_data['author'], form_data['ISBN_number'], form_data['year_published'], request.user.librarian.id, form_data['location']))
+
+        return redirect(reverse('libraryapp:books'))
