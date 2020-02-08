135f0bce33c7ec269bd8e5d7cba98b9c6f6c7596
John Long <jlcomp45@gmail.com>
2020-02-08 14:36:24 -0600

    completed chapter 7 and 8

--- libraryapp/views/__init__.py
+++ libraryapp/views/__init__.py
@@ -8,3 +8,5 @@ from .libraries.form import library_form
 from .books.details import book_details
 from .libraries.details import library_details
 from .librarians.details import librarian_details
+from .books.form import book_form
+from .books.form import book_edit_form
